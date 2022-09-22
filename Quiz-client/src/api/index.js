f2fb7af80c96cdbb0245221f863fbaba95c8de16
Gustavo Omonte Acevedo <gustavo.omonte@fundacion-jala.org>
2022-09-22 19:14:29 -0400

    Frontend: agregar endpoint para las preguntas

--- Quiz-client/src/api/index.js
+++ Quiz-client/src/api/index.js
@@ -4,6 +4,7 @@ export const BASE_URL = 'https://localhost:7112/';
 
 export const ENDPOINTS = {
   participant: 'participant',
+  question: 'question',
 };
 
 export const createAPIEndpoint = (endpoint) => {
